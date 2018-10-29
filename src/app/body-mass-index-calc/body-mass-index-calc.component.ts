611593c96d7146bde70b03c64face96d273c1082
John Lundgren <john.paul.lundgren@gmail.com>
2018-10-29 07:54:08 +0100

    Added BodyMassIndex component.

---
+++ src/app/body-mass-index-calc/body-mass-index-calc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-mass-index-calc',
+  templateUrl: './body-mass-index-calc.component.html',
+  styleUrls: ['./body-mass-index-calc.component.css']
+})
+export class BodyMassIndexCalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
