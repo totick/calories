7bd8f57b320e10dee937452ed235c5c4b1aabdbf
John Lundgren <john.paul.lundgren@gmail.com>
2018-10-30 11:24:27 +0100

    Added body mass index calculation in submit function.

--- src/app/body-mass-index-calc/body-mass-index-calc.component.ts
+++ src/app/body-mass-index-calc/body-mass-index-calc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-body-mass-index-calc',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyMassIndexCalcComponent implements OnInit {
 
+  bmiResult: number;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(f: NgForm){
+    let weight = +f.value.weight;
+    let height = (+f.value.height) / 100;
+    this.bmiResult = Math.round((weight / (height * height)) * 10) / 10;
   }
 
 }
